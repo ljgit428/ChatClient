70730078675675abb06388e12f4837b7a8896fe3
ljgit428 <l73jiang@gmail.com>
2019-05-17 11:21:33 -0400

    first commit

---
+++ ChatClient/ClientSocket.cpp
@@ -0,0 +1,34 @@
+﻿#include "stdafx.h"
+#include "ClientSocket.h"
+#include "ChatClientDlg.h"
+
+
+CClientSocket::CClientSocket()
+{
+}
+
+
+CClientSocket::~CClientSocket()
+{
+}
+
+
+void CClientSocket::OnReceive(int nErrorCode)
+{
+	// TODO:  在此添加专用代码和/或调用基类
+	Command cmd;
+	Receive(&cmd, sizeof(cmd));
+
+	CChatClientDlg* pMainDlg = (CChatClientDlg*)AfxGetMainWnd();
+
+
+	switch (cmd.cmd) {
+	case POSTNEWMESSAGE:{
+				  wchar_t newmessage[500] = {0};
+				  Receive(newmessage, sizeof(newmessage));
+				  pMainDlg->m_history.SetWindowText(newmessage);
+	}
+		break;
+	}
+	CSocket::OnReceive(nErrorCode);
+}
